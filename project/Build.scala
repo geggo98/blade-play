e5d925582d9c4d65d68575c7061f1c6fa2670dae
Stefan Schwetschke <stefan.schwetschke+github@googlemail.com>
2013-01-30 18:15:29 +0100

    Integrated Twitter bootstrap framework

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "Blade_night_App"
+  val appName         = "Blade_Night_App"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
@@ -13,9 +13,20 @@ object ApplicationBuild extends Build {
     anorm
   )
 
+  /**
+   * Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+   * [[https://github.com/playframework/Play20/wiki/Tips]]
+   */
+  def customLessEntryPoints(base: File): PathFinder = {
+    val bootstrap= base / "app" / "assets" / "stylesheets" / "bootstrap"
+    (bootstrap / "responsive.less") +++
+    (bootstrap / "bootstrap.less") +++
+    (base / "app" / "assets" / "stylesheets" * "*.less")
+  }
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
+    lessEntryPoints <<= baseDirectory(customLessEntryPoints)
   )
 
 }
